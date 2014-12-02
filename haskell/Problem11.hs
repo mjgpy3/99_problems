cb0a05dd5c14ddf950aa7bb333a0b45478be2a99
Michael Gilliland <mjg.py3@gmail.com>
2014-12-01 21:19:42 -0500

    Problem 11, haskell

---
+++ haskell/Problem11.hs
@@ -0,0 +1,19 @@
+module Problem11 where
+
+import Test.Hspec
+import Control.Exception
+import Problem10 (encode)
+
+data MaybeSingular a = Multiple Int a | Single a deriving (Show, Eq)
+
+encodeModified = aux.encode
+  where aux [] = []
+        aux (x:xs) = (case x of
+          (1, e) -> Single e
+          (n, e) -> Multiple n e):aux xs
+
+main :: IO ()
+main = hspec $ do
+  describe "encode" $ do
+    it "goin' for the gold!" $ do
+      encodeModified "aaaabccaadeeee" `shouldBe` [Multiple 4 'a',Single 'b',Multiple 2 'c', Multiple 2 'a',Single 'd',Multiple 4 'e']
