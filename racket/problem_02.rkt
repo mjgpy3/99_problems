90ac3bb8cb13ef883b80b520d10a95be71f63435
Michael Gilliland <mjg.py3@gmail.com>
2015-06-27 22:24:01 -0400

    problem 2

--- racket/problem_02.rkt
+++ racket/problem_02.rkt
@@ -1,3 +1,12 @@
 #lang racket
 
+(define (my-but-last xs)
+  (if (null? (cddr xs))
+    xs
+    (my-but-last (cdr xs))))
 
+(display (my-but-last '(1 3 4 2)))
+(newline)
+(display (my-but-last '(4 2)))
+(newline)
+(display (my-but-last '(4)))
